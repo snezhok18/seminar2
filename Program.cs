daabf4234d5484a1f8cc47af475493c01fa47f1f
Sergey <mma118@yandex.ru>
2022-12-25 10:13:37 +0800

    Added a solution problem1

--- Program.cs
+++ Program.cs
@@ -8,6 +8,40 @@
 // заданного числа или сообщает, что третьей цифры нет.
 // Берем числа до 100000.
 
+int num21 = new Random().Next(100000);
+
+Console.WriteLine($"Введено число {num21}");
+
+int num22 = num21;
+int count = 0;
+while (num21 != 0)
+{
+    num21 /= 10;
+    count++;
+}
+
+//Console.WriteLine($"В числе {count} разрядов.");
+
+if (count < 3)
+{
+    Console.WriteLine($"В числе {num22} нет третьей цифры");
+}
+if (count == 3)
+    {
+        int num23 = num22%10;
+        Console.WriteLine($"Третья цифра вашего числа {num23}.");
+    }
+if (count == 4)
+    {
+        int num23 = (num22/10)%10;
+        Console.WriteLine($"Третья цифра вашего числа {num23}.");
+    }    
+
+if (count == 5)
+    {
+        int num23 = (num22/100)%10;
+        Console.WriteLine($"Третья цифра вашего числа {num23}.");
+    }    
 
 // Задача 3:
 // Напишите программу, которая принимает на вход цифру, обозначающую
